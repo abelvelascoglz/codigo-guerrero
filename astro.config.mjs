dd4b8310993f0082348814c4ba41c98f6f69cf54
Abel Velasco <abelvelascoglz@gmail.com>
2025-06-26 18:37:44 -0400

    fixing astro.config.mjs for base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://abelvelascoglz.github.io',
-  base: '/codigo-guerrero',
+  base: '/codigo-guerrero/',
   integrations: [tailwind()],
   build: {
     assets: 'assets'
